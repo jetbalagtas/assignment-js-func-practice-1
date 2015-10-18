c8a6ac771fa009e94463f55bd0726d6f866fadbf
Jet Balagtas <jetbalagtas@gmail.com>
2015-10-18 00:01:09 -0400

    going through exercise-1

--- exercises-1.js
+++ exercises-1.js
@@ -24,8 +24,7 @@ function forEach(array, callback){
 // testing your code with console.assert
 var total = 1;
 forEach([1, 2, 3, 4], function(a){ total *= a; });
-  // for(var i = 0; i < a.length; i++);
-  // (a[i]);
+
 // and finally assert; if this fails, the program stops
 console.assert(total === 24);
 
@@ -38,9 +37,14 @@ function sum(){
 
     // .. do something with each element of args
     // YOUR CODE HERE
+    var sumTot = 0;
+    forEach(args, function(a){
+      sumTot += a;
+    });
+    return sumTot;
 }
 
-console.assert( sum(1, 2, 3, 4, 5) === 15 )
+console.assert( sum(1, 2, 3, 4, 5) === 15 );
 
 // 2. calculate the average of numbers (returns the average (A NUMBER))
 
@@ -50,9 +54,14 @@ function average(){
 
     // .. do something with each element of args
     // YOUR CODE HERE
+    var avgTot = 0;
+    forEach(args, function(a){
+      avgTot *= a;
+    });
+    return avgTot / args.length;
 }
 
-console.assert( average(2, 4, 6, 8) === 5 )
+console.assert( average(2, 4, 6, 8) === 5 );
 
 // 3. find the largest number of the inputs (returns the largest input (A NUMBER))
 
@@ -62,9 +71,18 @@ function largest(){
 
     // .. do something with each element of args
     // YOUR CODE HERE
+    // Reference: Thanks to Yamaha32088 for the non-exact solution, an example.
+    // Reference: http://stackoverflow.com/questions/13794225/finding-largest-integer-in-array-javascript
+    var lrg = 0;
+    forEach(args, function(a){
+      if (a > lrg) {
+        lrg = a;
+      }
+    });
+    return lrg;
 }
 
-console.assert( largest(2, 4, 6, 8) === 8 )
+console.assert( largest(2, 4, 6, 8) === 8 );
 
 // 4. find the longest string of the inputs (returns the longest input (A STRING))
 
@@ -74,6 +92,13 @@ function longest(){
 
     // .. do something with each element of args
     // YOUR CODE HERE
+    var long = 0;
+    forEach(args, function(a){
+      if (a > a.length) {
+        long = a;
+      }
+    });
+    return long;
 }
 
  console.assert( longest("this", "is", "a", "awesome", "function") === "function" );
